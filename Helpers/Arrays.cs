d3141083c6c081a06be22b38016f2b8e42958ec9
clara9621 <58587888+clara9621@users.noreply.github.com>
2020-01-15 11:28:28 +0200

    Add project files.

---
+++ Helpers/Arrays.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+
+namespace Helpers
+
+{
+
+    public class Arrays
+
+    {
+
+        public static int Length(int[] array)
+
+        {
+
+            return array.Length;
+
+        }
+
+        public static int Length(double[] array)
+
+        {
+
+            return array.Length;
+
+        }
+
+    }
+
+}
